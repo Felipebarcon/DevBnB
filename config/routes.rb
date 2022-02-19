d09f479c052afd22392fd1305b4bfac2ff5911f1
Stephane Perret <misochan1978@hotmail.com>
2022-02-19 16:54:00 +0100

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   resources :users do, only: [:create, :new]
-    resources :projects do, only: [:create, :new, :update, :destroy]
-      resources :orders, only: [:new, :create, :destroy]
+  end
+  resources :projects do
+    resources :orders, only: [:new, :create, :update]
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
