2430e978abcc6487601a68b2d6581d29b1b74bf8
Stephane Perret <misochan1978@hotmail.com>
2022-02-19 16:16:17 +0100

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  resources :users do
+    resources :projects do
+      resources :orders, only: [:post, :patch]
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
