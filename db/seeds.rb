680e2e76a40e1bea1b4207088950607ae08c8427
Alexandre Da Costa Monteiro <a.dcm33@gmail.com>
2022-02-22 20:39:14 +0100

    project seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# users = User.create([
+#   {email: "alex@email.com", encrypted_password: "password", username: "neiva", city: "Fribourg", created_at: "2022-02-22 18:00:00", updated_at: "2022-02-22 18:00:00"},
+#   {email: "felipe@email.com", encrypted_password: "password", username: "felbar", city: "Carouge", created_at: "2022-02-22 18:00:00", updated_at: "2022-02-22 18:00:00"},
+#   {email: "stephane@email.com", encrypted_password: "password", username: "misochan", city: "Lausanne", created_at: "2022-02-22 18:00:00", updated_at: "2022-02-22 18:00:00"}])
+projects = Project.create([
+  {name: "Landing page", description: "Simple landing page for your website", price: 150, duration: "3 days", user_id: 1, created_at: "2022-02-22 18:00:00", updated_at: "2022-02-22 18:00:00"},
+  {name: "Airbnb clone", description: "Airbnb clone for your business", price: 1000, duration: "1 month", user_id: 1, created_at: "2022-02-22 18:00:00", updated_at: "2022-02-22 18:00:00"},
+  {name: "Online store", description: "Multi language online store to grow your business", price: 4500, duration: "3 months", user_id: 1, created_at: "2022-02-22 18:00:00", updated_at: "2022-02-22 18:00:00"},
+  {name: "Useful API", description: "I can create the API you need in just a few days", price: 600, duration: "5 days", user_id: 1, created_at: "2022-02-22 18:00:00", updated_at: "2022-02-22 18:00:00"},
+  {name: "Relational database", description: "I can create any relational database you need, from simple to complex", price: 400, duration: "1 week", user_id: 1, created_at: "2022-02-22 18:00:00", updated_at: "2022-02-22 18:00:00"}
+  ])
