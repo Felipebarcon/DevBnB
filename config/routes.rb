bc633235437346587ab4a54de2a560715e872321
Felipe Barcon <felipe.barcon@gmail.com>
2022-02-22 20:01:28 +0100

    projects model ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :users, only: [:create, :new]
+
   resources :projects do
     resources :orders, only: [:new, :create, :update]
-     end
+  end
 end
