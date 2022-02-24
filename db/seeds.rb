604ecbf78a37e12fb86ebcd0cd2985d5e138ea04
Stephane Perret <misochan1978@hotmail.com>
2022-02-24 11:41:12 +0100

    adding new logo

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 users = User.create([
-   {email: "alex@email.com", encrypted_password: "password", username: "neiva", city: "Fribourg"},
+    {email: "alex@email.com", encrypted_password: "password", username: "neiva", city: "Fribourg"},
     {email: "felipe@email.com", encrypted_password: "password", username: "felbar", city: "Carouge"},
     {email: "stephane@email.com", encrypted_password: "password", username: "misochan", city: "Lausanne"}])
 
@@ -18,4 +18,3 @@ projects = Project.create([
    {name: "Useful API", description: "I can create the API you need in just a few days", price: 600, duration: "5 days", user_id: 1},
    {name: "Relational database", description: "I can create any relational database you need, from simple to complex", price: 400, duration: "1 week", user_id: 1}
    ])
-
